4341cbcf145f0cd0134b503dfe3bb38fe1fb8071
Jonathan <80016156+Jonathan929-del@users.noreply.github.com>
2022-10-25 16:10:16 +0200

    Commit

--- navigation/screens/MenuScreen.js
+++ navigation/screens/MenuScreen.js
@@ -1,9 +1,24 @@
 // Imports
+import {useEffect} from 'react';
+import * as Updates from 'expo-updates';
 import Menu from "../../components/Menu";
 
 
 // Main Function
 const MenuScreen = ({navigation}) => {
+
+  const reactToUpdates = async () => {
+    Updates.addListener(e => {
+      if(e.type === Updates.UpdateEventType.UPDATE_AVAILABLE){
+        Updates.reloadAsync();
+      }
+    });
+  };
+
+  useEffect(() => {
+    reactToUpdates();
+  }, []);
+
   return (
     <Menu navigation={navigation}/>
   )
